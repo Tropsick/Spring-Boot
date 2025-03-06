d5e5a67c2bf17159e625e3e2fc44da4ef38ea2c2
Tropsick <tropsick-for@ro.ru>
2025-03-06 19:53:05 +0300

    DB Server History

--- src/main/java/com/example/demo/controllers/KarmaController.java
+++ src/main/java/com/example/demo/controllers/KarmaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/karma")
+@RequestMapping("/api/karma")
 public class KarmaController {
     @Autowired
     private KarmaService karmaService;
